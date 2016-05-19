4fd51e0f723c6384ea40fa70efe2e639b7bd614c
iamsitting <cdsalamanca27@gmail.com>
2016-05-18 19:18:24 -0500

    Initial commit

---
+++ app/src/main/java/com/cxp/cyclexpro_v2/MainActivity.java
@@ -0,0 +1,51 @@
+package com.cxp.cyclexpro_v2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+public class MainActivity extends TitleBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.tvTitle.setText("Cycle X Pro");
+
+        Button solobtn = (Button) findViewById(R.id.solobtn);
+        Button trainbtn = (Button) findViewById(R.id.trainbtn);
+        Button racebtn = (Button) findViewById(R.id.racebtn);
+
+        //solobtn listener
+        solobtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, MetricsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //trainbtn listener
+        trainbtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, MetricsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //racebtn listener
+        racebtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, MetricsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cxp/cyclexpro_v2/TitleBarActivity.java
@@ -0,0 +1,28 @@
+package com.cxp.cyclexpro_v2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class TitleBarActivity extends Activity {
+    protected Button conbtn;
+    protected TextView tvTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
+
+        setContentView(R.layout.activity_title_bar);
+
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE,
+                R.layout.title_bar);
+
+        conbtn = (Button) findViewById(R.id.conbtn);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+    }
+}
